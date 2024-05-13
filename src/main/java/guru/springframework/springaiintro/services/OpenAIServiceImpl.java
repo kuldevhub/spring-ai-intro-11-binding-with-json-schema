7c14e285e17479af7688ccebfbe8561b674c6b9b
kuldevhub <66080378+kuldevhub@users.noreply.github.com>
2024-05-13 01:00:05 -0400

    Initial commit

---
+++ src/main/java/guru/springframework/springaiintro/services/OpenAIServiceImpl.java
@@ -0,0 +1,85 @@
+package guru.springframework.springaiintro.services;
+
+import java.util.Map;
+
+import org.springframework.ai.chat.ChatClient;
+import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.parser.BeanOutputParser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import guru.springframework.springaiintro.model.Answer;
+import guru.springframework.springaiintro.model.GetCapitalRequest;
+import guru.springframework.springaiintro.model.GetCapitalResponse;
+import guru.springframework.springaiintro.model.Question;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ */
+@Service
+public class OpenAIServiceImpl implements OpenAIService {
+
+    private final ChatClient chatClient;
+
+    public OpenAIServiceImpl(ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    @Value("classpath:templates/get-capital-prompt.st")
+    private Resource getCapitalPrompt;
+
+    @Value("classpath:templates/get-capital-with-info.st")
+    private Resource getCapitalPromptWithInfo;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Override
+    public Answer getCapitalWithInfo(GetCapitalRequest getCapitalRequest) {
+        PromptTemplate promptTemplate = new PromptTemplate(getCapitalPromptWithInfo);
+        Prompt prompt = promptTemplate.create(Map.of("stateOrCountry", getCapitalRequest.stateOrCountry()));
+        ChatResponse response = chatClient.call(prompt);
+
+        return new Answer(response.getResult().getOutput().getContent());
+    }
+
+    @Override
+    public GetCapitalResponse getCapital(GetCapitalRequest getCapitalRequest) {
+        BeanOutputParser<GetCapitalResponse> parser = new BeanOutputParser<>(GetCapitalResponse.class);
+        String format = parser.getFormat();
+
+        PromptTemplate promptTemplate = new PromptTemplate(getCapitalPrompt);
+        Prompt prompt = promptTemplate.create(Map.of("stateOrCountry", getCapitalRequest.stateOrCountry(),
+                "format", format));
+
+        ChatResponse response = chatClient.call(prompt);
+
+        return parser.parse(response.getResult().getOutput().getContent());
+    }
+
+    @Override
+    public Answer getAnswer(Question question) {
+        System.out.println("I was called");
+
+        PromptTemplate promptTemplate = new PromptTemplate(question.question());
+        Prompt prompt = promptTemplate.create();
+        ChatResponse response = chatClient.call(prompt);
+
+        return new Answer(response.getResult().getOutput().getContent());
+    }
+
+    @Override
+    public String getAnswer(String question) {
+        PromptTemplate promptTemplate = new PromptTemplate(question);
+        Prompt prompt = promptTemplate.create();
+        ChatResponse response = chatClient.call(prompt);
+
+        return response.getResult().getOutput().getContent();
+    }
+}
